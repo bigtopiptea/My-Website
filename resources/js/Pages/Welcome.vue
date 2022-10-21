42a04f085541dc0cd941ba2a5d8f33bfb9a4c610
Jesus Bermudo <janjanbermudo@gmail.com>
2022-10-21 21:44:05 +0800

    login register

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -15,6 +15,10 @@ export default {
         Head,
         Link
     },
+    props: {
+        canLogin: Boolean,
+        canRegister: Boolean,
+    },
     data() {
         return {
             isOpen: false,
@@ -88,7 +92,7 @@ export default {
                     </button>
 
                     <!-- Profile dropdown -->
-                    <div class="relative ml-3">
+                    <div v-if="$page.props.user" class="relative ml-3">
                         <div>
                             <button @click.prevent="dropdown" type="button"
                                 class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -99,16 +103,27 @@ export default {
                                     alt="">
                             </button>
                         </div>
-                        <div v-if="isOpen" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white px-1 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        <div v-if="isOpen"
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white px-1 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-2">Sign out</a>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700"
+                                role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700"
+                                role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700"
+                                role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                         </div>
+                    </div>
+                    <div v-else>
+                        <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in
+                        </Link>
+
+                        <Link v-if="canRegister" :href="route('register')"
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
                     </div>
                 </div>
             </div>
