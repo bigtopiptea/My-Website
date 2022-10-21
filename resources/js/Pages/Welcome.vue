09a4841c893efdfbb95802570f55232e7ba4835f
Jesus Bermudo <janjanbermudo@gmail.com>
2022-10-21 21:38:59 +0800

    try

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -99,14 +99,14 @@ export default {
                                     alt="">
                             </button>
                         </div>
-                        <div v-if="isOpen" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        <div v-if="isOpen" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-1">Settings</a>
-                            <a href="#" class="block px-4 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-2">Sign out</a>
                         </div>
                     </div>
