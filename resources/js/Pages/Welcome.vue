2a9b7a08b728427d0573d00e5923973192fabc71
Jesus Bermudo <janjanbermudo@gmail.com>
2022-10-21 21:54:00 +0800

    test

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -109,9 +109,9 @@ export default {
                                 class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700"
                                 role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
                             <form>
-                                <button @submit.prevent="logout"
+                                <a as="button" @submit.prevent="logout" 
                                     class="block px-4 py-2 text-sm hover:bg-gray-800 hover:text-white hover:rounded-md text-gray-700"
-                                    role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</button>
+                                    role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                             </form>
                         </div>
                     </div>
